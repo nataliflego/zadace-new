936b98e55c22fb197e8c4726625384fd8963fe86
nataliflego <60105977+nataliflego@users.noreply.github.com>
2021-11-25 21:00:28 +0100

    Add files via upload

---
+++ JS-405.js
@@ -0,0 +1,36 @@
+let natjecanje = {
+    naziv: "Pojedinačno prvenstvo Hrvatske za seniore i seniorke 2020",
+    datum_odrzavanja: "10.06.2021.",
+    lokacija: {
+        adresa: "Jarunska ul. 5",
+        postanski_broj: 10110,
+        grad: "Zagreb",
+        drzava: "Hrvatska"
+    },
+    organizatori: ["HAS","AK Dinamo-Zrinjevac"],
+    discipline: { 
+        trcanje: ["100m","200m","400m","800m","1500m","3000m"],
+        skokovi: ["Skok u vis","Skok u dalj","Troskok","Skok s motkom"],
+        bacanje: ["Bacanje kugle","Bacanje diska","Bacanje koplja"]
+    },
+    natjecatelji: [
+        {
+            ime: "Ivan",
+            prezime: "Ivić",
+            datum_rodenja: "22.01.1996.",
+            klub: "AK Dinamo-Zrinjevac"
+        },
+        {
+            ime: "Marta",
+            prezime: "Martić",
+            datum_rodenja: "15.08.1990.",
+            klub: "AK Istra Pula"
+        },
+        {
+            ime: "Luka",
+            prezime: "Lukić",
+            datum_rodenja: "25.10.1998.",
+            klub: "AK Istra Pula"
+        }
+    ]
+}
